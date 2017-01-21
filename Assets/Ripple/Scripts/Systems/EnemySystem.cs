2ae7308dc18b1ac1c1d76a784340b071af4b2593
Tim <tim@finergames.com>
2017-01-21 20:54:06 +0800

    - minor changes to the scene

---
+++ Assets/Ripple/Scripts/Systems/EnemySystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AlphaECS.Unity;
+using AlphaECS;
+
+public class EnemySystem : SystemBehaviour
+{
+	public float EnemyForce = 1f;
+	public float InfluenceRadius = 5f;
+
+	IGroup Rigidbodies;
+
+	public override void Setup ()
+	{
+		base.Setup ();
+
+//		foreach (var rbEntity in Rigidbodies.Entities)
+//		{
+//			var _rigidbody = rbEntity.GetComponent<Rigidbody> ();
+//			_rigidbody.AddExplosionForce(EnemyForce, position, RippleRadius, 0f, ForceMode.Impulse);
+//		}
+	}
+}
