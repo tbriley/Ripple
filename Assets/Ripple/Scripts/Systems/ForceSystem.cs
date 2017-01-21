ad79006658276579b54282e09e860d749958f539
Tim <tim@finergames.com>
2017-01-21 16:14:25 +0800

    - started properly breaking out force system to ecs style composition

--- Assets/Ripple/Scripts/Systems/ForceSystem.cs
+++ Assets/Ripple/Scripts/Systems/ForceSystem.cs
@@ -2,35 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using AlphaECS.Unity;
+using AlphaECS;
+using System;
+using UniRx;
+using UniRx.Triggers;
 
-public class InputSystem : SystemBehaviour
+public class ForceSystem : SystemBehaviour
 {
 	public GameObject Ground;
 	private Rigidbody Player;
 	public float ExplosionForce = 1f;
+	public float ExplosionRadius = 5f;
+	IGroup Rigidbodies;
 
 	public override void Setup ()
 	{
 		base.Setup ();
 
-		Player = GameObject.Find ("Player").GetComponent<Rigidbody> ();
+		Rigidbodies = GroupFactory.Create (new Type[]{ typeof(Rigidbody) });
 	}
 
 	void Update()
 	{
-//		if (!Input.GetMouseButton (0))
 		if (!Input.GetMouseButtonDown (0))
 			return;
 
 		var screenPosition = Input.mousePosition;
 		var ray = Camera.main.ScreenPointToRay (new Vector3(screenPosition.x, screenPosition.y, 0));
 
-		Debug.DrawRay (ray.origin, ray.direction * 1000f, Color.red);
 		RaycastHit raycastHit;
-
 		if (Physics.Raycast (ray, out raycastHit))
 		{
-			Player.AddExplosionForce (ExplosionForce, raycastHit.point, 5f, 0f, ForceMode.Impulse);
+			foreach (var entity in Rigidbodies.Entities)
+			{
+				var _rigidbody = entity.GetComponent<Rigidbody> ();
+				_rigidbody.AddExplosionForce(ExplosionForce, raycastHit.point, ExplosionRadius, 0f, ForceMode.Impulse);
+			}
 		}
 	}
 }
